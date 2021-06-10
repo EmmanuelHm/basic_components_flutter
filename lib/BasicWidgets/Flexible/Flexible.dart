bed8326f21de00f32b968556b7995223ff689ef2
Emmanuel Hernández Maqueda <emhermaq@gmail.com>
2021-06-10 18:34:46 -0500

    Widget 'Flexible' en flutter

---
+++ lib/BasicWidgets/Flexible/Flexible.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class FlexibleWidget extends StatelessWidget {
+  const FlexibleWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+            //Espacio que va a ocupar el widget
+            flex: 2,
+            //Hijo del Widget 'Flexible' => 'Container'
+            child: Container(
+              color: Colors.red,
+            )),
+        Flexible(
+            flex: 3,
+            child: Container(
+              color: Colors.blue,
+            )),
+        Flexible(
+            flex: 1,
+            child: Container(
+              color: Colors.green,
+            )),
+      ],
+    );
+  }
+}
